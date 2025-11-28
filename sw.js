c29ed351cfabb8c7bb11d44efe67a8e5e7d7cf20
negrete79 <116903139+negrete79@users.noreply.github.com>
2025-11-28 18:33:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,61 @@
 const CACHE_NAME = 'incubadora-pro-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './png192.png',
-    './png512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './png192.png',
+  './png512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
+// Instalação do Service Worker
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// Ativação do Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação de requisições
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Notificações push
+self.addEventListener('push', event => {
+  if (event.data) {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: './png192.png',
+      badge: './png192.png',
+      vibrate: [200, 100, 200]
+    });
+  }
 });
