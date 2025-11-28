71894dc932904a876f4b78474ceb0ff11802dd1b
negrete79 <116903139+negrete79@users.noreply.github.com>
2025-11-28 18:01:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,22 @@
-// Service Worker simplificado para GitHub Pages
 const CACHE_NAME = 'incubadora-pro-v1';
 const urlsToCache = [
     './',
     './index.html',
-    './style.css',
-    './app.js',
-    './ws.js',
     './manifest.json',
-    './png292.png',
-    './png540.png'
+    './png192.png',
+    './png512.png'
 ];
 
-// Instalar o Service Worker
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Ativar o Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Interceptação de requisições
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                
-                return fetch(event.request);
-            })
+            .then(response => response || fetch(event.request))
     );
 });
